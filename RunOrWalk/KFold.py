655316730509f40eb63682a9182e5c132efab77e
E0032186\JUSTIN TAN <e0032186@u.nus.edu>
2018-03-05 13:17:51 +0800

    updated excel file

--- RunOrWalk/KFold.py
+++ RunOrWalk/KFold.py
@@ -7,6 +7,7 @@ import numpy as np
 import timeit
 
 def KFoldalgo(X_list, y_list, trainer):
+    #num split selected as 50.
     kf = KFold(n_splits = 50)
     kf.get_n_splits(X_list)
     count = 0
@@ -17,13 +18,35 @@ def KFoldalgo(X_list, y_list, trainer):
         y_train, y_test = y_list[train_index], y_list[test_index]
         trainer.fit(X_train, y_train)
         y_pred = trainer.predict(X_test)
+        #compute the probability of success given (pred, and correct)
         pred_val = metrics.accuracy_score(y_test, y_pred)
+        #Sum the prediction up, to find average later.
         final_accuracy = final_accuracy + pred_val
         count = count +1
+
+        
     trainer.fit(X_list, y_list)
+
+    #sample first 1st set of data(50 sampling points)
     sample_data = X_list[0:1, 0:]
     start_time = timeit.default_timer()
     pred_one = trainer.predict(sample_data)
     total_time = (timeit.default_timer() - start_time)*1000
-    print ("Time taken for one prediction: {0:.50f}".format(total_time))
+    print ("Time taken for one prediction in ms: {0:.50f}".format(total_time))
+    print ("Prediction for current algorithm, 1st window(0 = Walk, 1 = Jog, 2 = Run): {}".format(pred_one))
+    print ("label for current algorithm, 1st window: [{}]".format(y_list[0]))
+
+    #sample 8500/25 = 340
+    sample_data = X_list[339:340, 0:]
+    pred_two = trainer.predict(sample_data)
+    print ("Prediction for current algorithm, 340th window(0 = Walk, 1 = Jog, 2 = Run): {}".format(pred_two))
+    print ("label for current algorithm, 340th window: [{}]".format(y_list[339]))
+
+    #sample 725/25 = 29
+    sample_data = X_list[29:30, 0:]
+    pred_three = trainer.predict(sample_data)
+    print ("Prediction for current algorithm, 30th window(0 = Walk, 1 = Jog, 2 = Run): {}".format(pred_three))
+    print ("label for current algorithm, 30th window: [{}]".format(y_list[29]))
+
+    #returns accuracy average
     return final_accuracy/ count
--- RunOrWalk/nb_author_id.py
+++ RunOrWalk/nb_author_id.py
@@ -29,14 +29,15 @@ def preprocesses():
     data = df1.values
     total_sample = len(data)
     portion = 1
+    #Sample data are from 2nd column onwards
     X = data[0:int(total_sample*portion), 1:]
+    #Labels are given only in the first column
     y = data[0:int(total_sample*portion), 0]
     #segment the data by window size and overlap
     window_size = 50;
     overlap = 25;
     segment = []
     labels = []
-    print len(X)
     for i in range(len(X)/overlap):
         segment.append(X[i*overlap:((i*overlap)+(window_size)), 0:])        
         labels.append(y[i*overlap:((i*overlap)+(window_size))])    
@@ -47,9 +48,13 @@ def preprocesses():
         temp_row = []
         for j in range(0,6):
             temp = segment[i][0:,j]
+            #Mean = sum of everything / no. of data point
             mean = np.mean(temp)
+            #Median = middle value of sorted
             median = np.median(temp)
+            #Std = Standard Deviation, How individual points differs from the mean
             std = np.std(temp)
+            #iqr = Inter-Quartile Range, 75th percentile - 25th percentile
             iqr = np.percentile(temp, [75 ,25])
             q75, q25 = np.percentile(temp, [75 ,25])
             iqr = q75 - q25
@@ -60,6 +65,7 @@ def preprocesses():
             temp_row.append(iqr)
             temp_row.append(maximum)
             Fourier_temp = fft(temp)
+            #Fourier = Power Spectral Density, essentially, Summation |Ck|^2
             fourier = np.abs(Fourier_temp)**2
             value = 0
             for x in range (len(fourier)):
@@ -72,13 +78,19 @@ def preprocesses():
     for i in range(len(labels)):
         mean = np.mean(labels[i])
         if(mean >=0.666):
+            #if majority is "1", it means running, (2)
             label_list.append(int(2))
         elif(mean >=0.333):
+            #if half is "1", half is "0", jogging (1)
             label_list.append(int(1))
         else:
+            #if majority is "0", resting (0)
             label_list.append(int(0))
     #normalizing the features
     stat_list = preprocessing.normalize(stat_list)
+    #use this code to extract top 5 features only
+    #stat_list = stat_list[0: ,[5,7,10,11,35]]
+    
     #X_train, X_test, y_train, y_test = train_test_split(stat_list, label_list,train_size=0.75,test_size=0.25, random_state=42)
     #scaler = StandardScaler()
     #scaler.fit(X_train)
